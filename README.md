fbe40535712eb02e27f3c90cadefb171c3650313
framrt <69787373+framrt@users.noreply.github.com>
2025-07-20 12:30:13 +0200

    first update

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# EASYCHAT
+
+This is a easy way to communicate in local using socket. 
+
+The final goal is to create a windows using just C and a communication channel using a home PC as destination server. 
+
+Compile instruction: 
+
+gcc tcpServer.c -o server
+
+gcc tcpClient.c -o client 
+
+
+firstly execute the server ./server command, then type 
+./client, so the communication has been established. 
+
+
+
+//For suggestion and idea don't esitate to contact me 
---
+++ tcpClient.c
@@ -0,0 +1,68 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+#include <stdbool.h>
+void func(int sockfd)
+{
+    char buff[MAX];
+    int n;
+    while (true) {
+        bzero(buff, sizeof(buff));
+        printf("Enter the string : ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        write(sockfd, buff, sizeof(buff));
+        bzero(buff, sizeof(buff));
+        read(sockfd, buff, sizeof(buff));
+        printf("From Server : %s", buff);
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    //crea er socket 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket succ created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    //assegna porta??? 
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+    
+    //connetti both tramite socket 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) //cast necessary
+        != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    // function for chat
+    func(sockfd);
+
+    // close the socket
+    close(sockfd);
+}
